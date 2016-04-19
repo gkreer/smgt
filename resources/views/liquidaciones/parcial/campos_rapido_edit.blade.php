66870d5c0f0bc3824e21f22493b011809c754d1a
gkreer <gkreer@gmail.com>
2016-04-18 21:57:34 -0300

    Removidos campos que no se utilizan

--- resources/views/liquidaciones/parcial/campos_rapido_edit.blade.php
+++ resources/views/liquidaciones/parcial/campos_rapido_edit.blade.php
@@ -36,22 +36,6 @@
                   {!! Form::label('kmsAlqEntrada', 'KMS Alquilados') !!}
                     {!! Form::number('kmsAlqSalida', null, ['class' => 'form-control']) !!}
                   </td>
-                  <td>
-                  {!! Form::label('bandDiurnaEntrada', 'Viajes Diurnos') !!}
-                    {!! Form::number('bandDiurnaSalida', null, ['class' => 'form-control']) !!}
-                  </td>
-                  <td>
-                  {!! Form::label('fichaDiurnaEntrada', 'Fichas Diurnas') !!}
-                    {!! Form::number('fichaDiurnaSalida', null, ['class' => 'form-control']) !!}
-                  </td>
-                  <td>
-                  {!! Form::label('bandNocturnaEntrada', 'Viajes Nocturnos') !!}
-                    {!! Form::number('bandNocturnaSalida', null, ['class' => 'form-control']) !!}
-                  </td>
-                  <td>
-                  {!! Form::label('fichaNocturnaEntrada', 'Fichas Nocturnas') !!}
-                    {!! Form::number('fichaNocturnaSalida', null, ['class' => 'form-control']) !!}
-                  </td>
               </tr>
               </table>
             </div>
